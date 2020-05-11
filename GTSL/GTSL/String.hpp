1a6b38038c503180a253c286089f7c0e0e3e2657
Facundo961 <facundovilla961@gmail.com>
2020-05-11 00:04:16 -0300

    Added ranger to String.

--- GTSL/GTSL/String.hpp
+++ GTSL/GTSL/String.hpp
@@ -82,6 +82,8 @@ namespace GTSL
 		//Returns the contents of this String as a C-String.
 		char* c_str() { return data.GetData(); }
 
+		operator Ranger<UTF8>() const { return Ranger<UTF8>(data); }
+		
 		//Returns the contents of this String as a C-String.
 		[[nodiscard]] const char* c_str() const { return data.GetData(); }
 
