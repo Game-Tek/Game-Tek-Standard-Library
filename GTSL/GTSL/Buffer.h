a5c87848d5f97ae46228cd3fc1a3cbc97958b61d
Facundo Villa <facundovilla961@gmail.com>
2020-07-14 19:19:42 -0300

    Fixed buffer.

--- GTSL/GTSL/Buffer.h
+++ GTSL/GTSL/Buffer.h
@@ -28,27 +28,32 @@ namespace GTSL
 			capacity = allocated_size;
 		}
 
-		void Resize(const uint32 size) { length = size; }
+		void Resize(const uint32 size) { length = size; pos = size; }
 		
 		void Free(const uint32 alignment, const AllocatorReference& allocatorReference)
 		{
 			allocatorReference.Deallocate(capacity, alignment, data); capacity = 0; data = nullptr;
 		}
 
-		void WriteBytes(const uint32 size, byte* from) 
+		void WriteBytes(const uint32 size, const byte* from) 
 		{
-			MemCopy(size, from, data + length);
-			length += size;
+			GTSL_ASSERT(pos + size <= capacity, "Can't fit more!");
+			MemCopy(size, from, data + pos);
+			pos += size;
 		}
 
 		void ReadBytes(const uint32 size, byte* to)
 		{
-			MemCopy(size, data + (capacity - length), to);
-			length -= size;
+			GTSL_ASSERT(pos != 0, "Buffer is already empty!");
+			MemCopy(size, data + (length - pos), to);
+			pos -= size;
 		}
+
+		[[nodiscard]] uint32 GetRemainingSize() const { return length - pos; }
 		
 		[[nodiscard]] uint32 GetCapacity() const { return capacity; }
 		[[nodiscard]] uint32 GetLength() const { return length; }
+		[[nodiscard]] uint32 GetPosition() const { return pos; }
 		[[nodiscard]] byte* GetData() const { return data; }
 
 		operator Ranger<byte>() const { return Ranger<byte>(length, data); }
@@ -58,12 +63,14 @@ namespace GTSL
 		byte* data{ nullptr };
 		uint32 capacity{ 0 };
 		uint32 length{ 0 };
+		uint32 pos = 0;
 
 		friend void Insert(bool n, Buffer& buffer, const AllocatorReference& allocatorReference);
 		friend void Insert(uint32 n, Buffer& buffer, const AllocatorReference& allocatorReference);
 		friend void Insert(uint64 n, Buffer& buffer, const AllocatorReference& allocatorReference);
 		friend void Insert(float32 n, Buffer& buffer, const AllocatorReference& allocatorReference);
 		friend void Extract(bool& n, Buffer& buffer, const AllocatorReference& allocatorReference);
+		friend void Extract(uint16& n, Buffer& buffer, const AllocatorReference& allocatorReference);
 		friend void Extract(uint32& n, Buffer& buffer, const AllocatorReference& allocatorReference);
 		friend void Extract(uint64& n, Buffer& buffer, const AllocatorReference& allocatorReference);
 		friend void Extract(float32& n, Buffer& buffer, const AllocatorReference& allocatorReference);
@@ -71,49 +78,46 @@ namespace GTSL
 	
 	inline void Extract(bool& n, Buffer& buffer, const AllocatorReference& allocatorReference)
 	{
-		GTSL_ASSERT(buffer.length != 0, "Buffer is already empty!");
-		n = *reinterpret_cast<bool*>(buffer.data + buffer.length); buffer.length -= sizeof(bool);
+		buffer.ReadBytes(sizeof(bool), reinterpret_cast<byte*>(&n));
 	}
-	
+
+	inline void Extract(uint16& n, Buffer& buffer, const AllocatorReference& allocatorReference)
+	{
+		buffer.ReadBytes(sizeof(uint16), reinterpret_cast<byte*>(&n));
+	}
+
 	inline void Extract(uint32& n, Buffer& buffer, const AllocatorReference& allocatorReference)
 	{
-		GTSL_ASSERT(buffer.length != 0, "Buffer is already empty!");
-		n = *reinterpret_cast<uint32*>(buffer.data + buffer.length); buffer.length -= sizeof(uint32);
+		buffer.ReadBytes(sizeof(uint32), reinterpret_cast<byte*>(&n));
 	}
 
 	inline void Extract(uint64& n, Buffer& buffer, const AllocatorReference& allocatorReference)
 	{
-		GTSL_ASSERT(buffer.length != 0, "Buffer is already empty!");
-		n = *reinterpret_cast<uint64*>(buffer.data + buffer.length); buffer.length -= sizeof(uint64);
+		buffer.ReadBytes(sizeof(uint64), reinterpret_cast<byte*>(&n));
 	}
 
 	inline void Extract(float32& n, Buffer& buffer, const AllocatorReference& allocatorReference)
 	{
-		GTSL_ASSERT(buffer.length != 0, "Buffer is already empty!");
-		n = *reinterpret_cast<float32*>(buffer.data + buffer.length); buffer.length -= sizeof(float32);
+		buffer.ReadBytes(sizeof(float32), reinterpret_cast<byte*>(&n));
 	}
 
 	inline void Insert(const bool n, Buffer& buffer, const AllocatorReference& allocatorReference)
 	{
-		GTSL_ASSERT(buffer.length + sizeof(bool) <= buffer.capacity, "Buffer has no more space!");
-		*reinterpret_cast<bool*>(buffer.data + buffer.length) = n; buffer.length += sizeof(bool);
+		buffer.WriteBytes(sizeof(bool), reinterpret_cast<const byte*>(&n));
 	}
 
 	inline void Insert(const uint32 n, Buffer& buffer, const AllocatorReference& allocatorReference)
 	{
-		GTSL_ASSERT(buffer.length + sizeof(uint32) <= buffer.capacity, "Buffer has no more space!");
-		*reinterpret_cast<uint32*>(buffer.data + buffer.length) = n; buffer.length += sizeof(uint32);
+		buffer.WriteBytes(sizeof(uint32), reinterpret_cast<const byte*>(&n));
 	}
 
 	inline void Insert(const uint64 n, Buffer& buffer, const AllocatorReference& allocatorReference)
 	{
-		GTSL_ASSERT(buffer.length + sizeof(uint64) <= buffer.capacity, "Buffer has no more space!");
-		*reinterpret_cast<uint64*>(buffer.data + buffer.length) = n; buffer.length += sizeof(uint64);
+		buffer.WriteBytes(sizeof(uint64), reinterpret_cast<const byte*>(&n));
 	}
 
 	inline void Insert(const float32 n, Buffer& buffer, const AllocatorReference& allocatorReference)
 	{
-		GTSL_ASSERT(buffer.length + sizeof(float32) <= buffer.capacity, "Buffer has no more space!");
-		*reinterpret_cast<float32*>(buffer.data + buffer.length) = n; buffer.length += sizeof(float32);
+		buffer.WriteBytes(sizeof(float32), reinterpret_cast<const byte*>(&n));
 	}
 }
