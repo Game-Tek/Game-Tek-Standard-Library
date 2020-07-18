e15e91fe3f2a276e7008701fdd4f8c80eaee4714
Facundo Villa <facundovilla961@gmail.com>
2020-07-18 03:09:07 -0300

    Added SmartPointer.

--- GTSL/GTSL/Allocator.h
+++ GTSL/GTSL/Allocator.h
@@ -12,9 +12,8 @@ namespace GTSL
 	{
 		AllocatorReference() = default;
 
-		AllocatorReference(void(AllocatorReference::*allocateFunc)(uint64, uint64, void**, uint64*) const, void(AllocatorReference::* deallocateFunc)(uint64, uint64, void*) const) : allocate(allocateFunc), deallocate(deallocateFunc)
-		{
-		}
+		AllocatorReference(const AllocatorReference& allocatorReference) = default;
+		AllocatorReference(AllocatorReference&& allocatorReference) = default;
 
 		/**
 		 * \brief Allocates memory.
@@ -23,7 +22,6 @@ namespace GTSL
 		 * \param data Pointer to a pointer to store the allocation.
 		 * \param allocatedSize Pointer to store the allocated size, this will be at least as big as size. This is done as sometimes allocators allocate more space than what it was asked to, this way the client can take advantage of this and less memory is wasted.
 		 */
-		void Allocate(const uint64 size, const uint64 alignment, void** data, uint64 * allocatedSize) const { (this->*allocate)(size, alignment, data, allocatedSize); }
 
 		/**
 		 * \brief Allocates memory.
@@ -31,7 +29,6 @@ namespace GTSL
 		 * \param alignment Alignment of the allocation.
 		 * \param data Pointer to a pointer to store the allocation.
 		 */
-		void Allocate(const uint64 size, const uint64 alignment, void** data) const { uint64 allocated_size{ 0 }; (this->*allocate)(size, alignment, data, &allocated_size); }
 
 		/**
 		 * \brief Deallocates allocated memory.
@@ -39,67 +36,59 @@ namespace GTSL
 		 * \param alignment Alignment of the allocation being freed.
 		 * \param data Pointer to the memory block being freed.
 		 */
-		void Deallocate(const uint64 size, const uint64 alignment, void* data) const { (this->*deallocate)(size, alignment, data); }
-		
-	protected:
-		void(AllocatorReference::*allocate)(uint64, uint64, void**, uint64*)const{ nullptr };
-		void(AllocatorReference::*deallocate)(uint64, uint64, void*)const{ nullptr };
 	};
 
-	template<typename T>
-	struct Allocation
+	template<typename T, class ALLOCATOR>
+	struct SmartPointer
 	{
-		Allocation() = default;
+		SmartPointer() = default;
 		
-		Allocation(const Allocation& other) : Size(other.Size), Alignment(other.Alignment), Data(other.Data)
+		SmartPointer(const SmartPointer& other) : size(other.size), alignment(other.alignment), data(other.data)
 		{
 		}
 
-		Allocation(Allocation&& other) noexcept : Size(other.Size), Alignment(other.Alignment), Data(other.Data)
+		SmartPointer(SmartPointer&& other) noexcept : size(other.size), alignment(other.alignment), data(other.data)
 		{
-			other.Data = nullptr;
+			other.data = nullptr;
 		}
 #if (!_DEBUG)
 		~Allocation() = default;
 #else
-		~Allocation() { GTSL_ASSERT(!this->Data, "Data was not freed!"); }
+		~SmartPointer() { if (this->data) { this->allocator.Deallocate(this->size, this->alignment, this->data); } }
 #endif
 
-		Allocation& operator=(const Allocation& other)
+		SmartPointer& operator=(const SmartPointer& other)
 		{
-			Size = other.Size; Alignment = other.Alignment; Data = other.Data; return *this;
+			size = other.size; alignment = other.alignment; data = other.data; return *this;
 		}
 
-		Allocation& operator=(Allocation&& other) noexcept
+		SmartPointer& operator=(SmartPointer&& other) noexcept
 		{
-			Size = other.Size; Alignment = other.Alignment; Data = other.Data;
-			other.Data = nullptr;
+			size = other.size; alignment = other.alignment; data = other.data;
+			other.data = nullptr;
 			return *this;
 		}
-				
-		uint32 Size{ 0 };
-		uint32 Alignment{ 0 };
-		T* Data{ nullptr };
-
-		operator T*() const { return Data; }
-		T* operator->() const { return Data; }
-		T& operator*() const { return *Data; }
+		
+		operator T*() const { return data; }
+		T* operator->() const { return data; }
+		T& operator*() const { return *data; }
 
 		template<typename TT, typename... ARGS>
-		static Allocation<T> Create(const AllocatorReference& allocatorReference, ARGS&&... args)
+		static SmartPointer<T, ALLOCATOR> Create(const ALLOCATOR& allocatorReference, ARGS&&... args)
 		{
-			Allocation<T> ret; ret.Size = sizeof(TT); ret.Alignment = alignof(TT);
-			allocatorReference.Allocate(sizeof(TT), alignof(TT), reinterpret_cast<void**>(&ret.Data));
-			::new(ret.Data) TT(MakeForwardReference<ARGS>(args)...); return ret;
+			T* data = nullptr; allocatorReference.Allocate(sizeof(TT), alignof(TT), reinterpret_cast<void**>(&data));
+			return SmartPointer<T, ALLOCATOR>(sizeof(TT), alignof(TT), data, allocatorReference);
 		}
 
-		template<typename TT, typename... ARGS>
-		static void Create(const AllocatorReference& allocatorReference, Allocation& allocation, ARGS&&... args)
-		{
-			allocation.Size = sizeof(TT); allocation.Alignment = alignof(TT);
-			allocatorReference.Allocate(sizeof(TT), alignof(TT), reinterpret_cast<void**>(&allocation.Data));
-			::new(allocation.Data) TT(MakeForwardReference<ARGS>(args)...);
-		}
+	private:
+		SmartPointer(const uint32 size, const uint32 alignment, T* object, const ALLOCATOR& allocatorReference) : size(size), alignment(alignment), data(object), allocator(allocatorReference)
+		{}
+		
+		uint32 size{ 0 };
+		uint32 alignment{ 0 };
+		T* data{ nullptr };
+
+		[[no_unique_address]] ALLOCATOR allocator;
 	};
 
 	template<typename T, typename ...ARGS>
@@ -109,16 +98,6 @@ namespace GTSL
 		allocatorReference.Allocate(sizeof(T), alignof(T), &data, &size);
 		::new(data) T(MakeForwardReference<ARGS>(args)...);
 		return static_cast<T*>(data);
-	}
-	
-	template<typename T>
-	void Delete(Allocation<T>& allocation, const AllocatorReference& allocatorReference)
-	{
-		allocation->~T();
-		allocatorReference.Deallocate(allocation.Size, allocation.Alignment, static_cast<void*>(allocation.Data));
-#if (_DEBUG)
-		allocation.Data = nullptr;
-#endif
 	}
 
 	template<typename T>
