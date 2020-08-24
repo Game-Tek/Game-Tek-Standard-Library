89200d24f8b22fba7f142965dd0fcfc57af003ef
Facundo Villa <facundovilla961@gmail.com>
2020-08-24 15:15:21 -0300

    Fixed ToString returns.

--- GTSL/src/StringCommon.cpp
+++ GTSL/src/StringCommon.cpp
@@ -4,50 +4,50 @@
 
 void GTSL::ToString(const int8 num, Ranger<UTF8>& buffer)
 {
-	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%c", num), buffer.begin());
+	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%c", num) + 1, buffer.begin());
 }
 
 void GTSL::ToString(const uint8 num, Ranger<UTF8>& buffer)
 {
-	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%c", num), buffer.begin());
+	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%c", num) + 1, buffer.begin());
 }
 
 void GTSL::ToString(const int16 num, Ranger<UTF8>& buffer)
 {
-	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%hi", num), buffer.begin());
+	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%hi", num) + 1, buffer.begin());
 }
 
 void GTSL::ToString(const uint16 num, Ranger<UTF8>& buffer)
 {
-	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%hu", num), buffer.begin());
+	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%hu", num) + 1, buffer.begin());
 }
 
 void GTSL::ToString(const int32 num, Ranger<UTF8>& buffer)
 {
-	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%d", num), buffer.begin());
+	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%d", num) + 1, buffer.begin());
 }
 
 void GTSL::ToString(const uint32 num, Ranger<UTF8>& buffer)
 {
-	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%u", num), buffer.begin());
+	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%u", num) + 1, buffer.begin());
 }
 
 void GTSL::ToString(const int64 num, Ranger<UTF8>& buffer)
 {
-	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%lld", num), buffer.begin());
+	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%lld", num) + 1, buffer.begin());
 }
 
 void GTSL::ToString(const uint64 num, Ranger<UTF8>& buffer)
 {
-	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%llu", num), buffer.begin());
+	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%llu", num) + 1, buffer.begin());
 }
 
 void GTSL::ToString(const float32 num, Ranger<UTF8>& buffer)
 {
-	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%f", num), buffer.begin());
+	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%f", num) + 1, buffer.begin());
 }
 
 void GTSL::ToString(const float64 num, Ranger<UTF8>& buffer)
 {
-	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%lf", num), buffer.begin());
+	buffer = Ranger<UTF8>(sprintf_s(buffer.begin(), buffer.end() - buffer.begin(), "%lf", num) + 1, buffer.begin());
 }
