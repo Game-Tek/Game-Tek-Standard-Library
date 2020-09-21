9b2815f259c07c8b54dca4b6acc7ed4071ae32f3
Facundo Villa <facundovilla961@gmail.com>
2020-09-21 02:02:25 -0300

    Added ray tracing primitives.

--- GAL/GAL/Vulkan/VulkanAccelerationStructures.h
+++ GAL/GAL/Vulkan/VulkanAccelerationStructures.h
@@ -2,9 +2,25 @@
 
 #define VK_ENABLE_BETA_EXTENSIONS
 #include "Vulkan.h"
+#include "GTSL/Ranger.h"
 
 namespace GAL
 {
+	struct AccelerationStructureGeometry
+	{
+		VulkanGeometryFlags::value_type Flags;
+		VulkanGeometryType Type;
+		VulkanShaderDataType VertexFormat;
+		GTSL::uint32 VertexStride;
+		VulkanIndexType IndexType;
+		VulkanDeviceAddress VertexData, IndexData;
+	};
+
+	struct BuildOffset
+	{
+		GTSL::uint32 PrimitiveCount = 0, PrimitiveOffset = 0, FirstVertex = 0, TransformOffset = 0;
+	};
+	
 	class VulkanAccelerationStructure
 	{
 	public:
@@ -43,21 +59,53 @@ namespace GAL
 			GTSL::uint32 PrimitiveOffset;
 			GTSL::uint32 TransformOffset;
 		};
-		
+
+		struct TopLevelCreateInfo : VulkanCreateInfo
+		{
+			VulkanAccelerationStructureFlags::value_type Flags;
+			AccelerationStructureGeometryType GeometryType;
+			GTSL::uint32 MaxGeometryCount = 0;
+			GTSL::uint32 CompactedSize = 0;
+			VulkanDeviceAddress DeviceAddress;
+		};
+
+		struct BottomLevelCreateInfo : VulkanCreateInfo
+		{
+			AccelerationStructureGeometryType GeometryType;
+			GTSL::uint32 MaxGeometryCount = 0;
+			GTSL::uint32 CompactedSize = 0;
+			VulkanDeviceAddress DeviceAddress;
+		};
+		VulkanAccelerationStructure(const BottomLevelCreateInfo& info);
+
+
+		void Destroy(const VulkanRenderDevice* renderDevice);
+
+		//UTILITY
+		static void BuildAccelerationStructure(const struct BuildAccelerationStructuresInfo& info);
 	protected:
 		VkAccelerationStructureKHR accelerationStructure;
 	};
-	
-	class VulkanTopLevelAccelerationStructure : public VulkanAccelerationStructure
+
+	struct BuildAccelerationStructureInfo : VulkanRenderInfo
 	{
-	public:
-		VulkanTopLevelAccelerationStructure() = default;
-		struct CreateInfo : VulkanCreateInfo
-		{
-			
-		};
-		VulkanTopLevelAccelerationStructure(const CreateInfo& createInfo);
-		void Destroy(VulkanRenderDevice* renderDevice);
 	private:
+		GTSL::uint32 stype; void* next;
+	public:
+		GTSL::uint32 IsTopLevelFalse = false;
+		GTSL::uint32 Flags = false;
+		GTSL::uint32 Update = false;
+		VulkanAccelerationStructure SourceAccelerationStructure;
+		VulkanAccelerationStructure DestinationAccelerationStructure;
+		GTSL::uint32 IsArrayOfPointers = false;
+		GTSL::uint32 Count = 0;
+		const AccelerationStructureGeometry* Geometries;
+		VulkanDeviceAddress ScratchBufferAddress;
+	};
+
+	struct BuildAccelerationStructuresInfo : VulkanRenderInfo
+	{
+		GTSL::Ranger<const BuildAccelerationStructureInfo> BuildAccelerationStructureInfos;
+		const BuildOffset* const* BuildOffsets;
 	};
 }
--- GAL/src/Vulkan/VulkanAccelerationStructures.cpp
+++ GAL/src/Vulkan/VulkanAccelerationStructures.cpp
@@ -2,25 +2,56 @@
 
 #include "GAL/Vulkan/VulkanRenderDevice.h"
 
-GAL::VulkanTopLevelAccelerationStructure::VulkanTopLevelAccelerationStructure(const CreateInfo& createInfo)
+GAL::VulkanAccelerationStructure::VulkanAccelerationStructure(const TopLevelCreateInfo& info)
 {
 	VkAccelerationStructureCreateGeometryTypeInfoKHR acceleration_structure_create_geometry_type{ VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_CREATE_GEOMETRY_TYPE_INFO_KHR };
 	acceleration_structure_create_geometry_type.geometryType = VK_GEOMETRY_TYPE_TRIANGLES_KHR;
-	acceleration_structure_create_geometry_type.indexType;
-	acceleration_structure_create_geometry_type.allowsTransforms;
-	acceleration_structure_create_geometry_type.maxPrimitiveCount;
-	acceleration_structure_create_geometry_type.maxVertexCount;
-	acceleration_structure_create_geometry_type.vertexFormat;
+	acceleration_structure_create_geometry_type.indexType = static_cast<VkIndexType>(info.GeometryType.IndexType);
+	acceleration_structure_create_geometry_type.allowsTransforms = info.GeometryType.AllowTransforms;
+	acceleration_structure_create_geometry_type.maxPrimitiveCount = info.GeometryType.MaxPrimitiveCount;
+	acceleration_structure_create_geometry_type.maxVertexCount = info.GeometryType.MaxVertexCount;
+	acceleration_structure_create_geometry_type.vertexFormat = static_cast<VkFormat>(info.GeometryType.VertexType);
 	
-	VkAccelerationStructureCreateInfoKHR vk_acceleration_structure_create_info_khr{ VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_CREATE_INFO_KHR };
-	vk_acceleration_structure_create_info_khr.type = VK_ACCELERATION_STRUCTURE_TYPE_TOP_LEVEL_KHR;
-	//vk_acceleration_structure_create_info_khr.
-	
-	createInfo.RenderDevice->vkCreateAccelerationStructureKHR(createInfo.RenderDevice->GetVkDevice(), &vk_acceleration_structure_create_info_khr, createInfo.RenderDevice->GetVkAllocationCallbacks(), &accelerationStructure);
-	SET_NAME(accelerationStructure, VK_OBJECT_TYPE_ACCELERATION_STRUCTURE_KHR, createInfo);
+	VkAccelerationStructureCreateInfoKHR vkAccelerationStructureCreateInfoKhr{ VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_CREATE_INFO_KHR };
+	vkAccelerationStructureCreateInfoKhr.flags = info.Flags;
+	vkAccelerationStructureCreateInfoKhr.type = VK_ACCELERATION_STRUCTURE_TYPE_TOP_LEVEL_KHR;
+	vkAccelerationStructureCreateInfoKhr.pGeometryInfos = &acceleration_structure_create_geometry_type;
+	vkAccelerationStructureCreateInfoKhr.maxGeometryCount = info.MaxGeometryCount;
+	vkAccelerationStructureCreateInfoKhr.compactedSize = info.CompactedSize;
+	vkAccelerationStructureCreateInfoKhr.deviceAddress = info.DeviceAddress;
+
+	info.RenderDevice->vkCreateAccelerationStructureKHR(info.RenderDevice->GetVkDevice(), &vkAccelerationStructureCreateInfoKhr, info.RenderDevice->GetVkAllocationCallbacks(), &accelerationStructure);
+	SET_NAME(accelerationStructure, VK_OBJECT_TYPE_ACCELERATION_STRUCTURE_KHR, info)
 }
 
-void GAL::VulkanTopLevelAccelerationStructure::Destroy(VulkanRenderDevice* renderDevice)
+GAL::VulkanAccelerationStructure::VulkanAccelerationStructure(const BottomLevelCreateInfo& info)
+{
+	VkAccelerationStructureCreateGeometryTypeInfoKHR acceleration_structure_create_geometry_type{ VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_CREATE_GEOMETRY_TYPE_INFO_KHR };
+	acceleration_structure_create_geometry_type.geometryType = VK_GEOMETRY_TYPE_TRIANGLES_KHR;
+	acceleration_structure_create_geometry_type.indexType = static_cast<VkIndexType>(info.GeometryType.IndexType);
+	acceleration_structure_create_geometry_type.allowsTransforms = info.GeometryType.AllowTransforms;
+	acceleration_structure_create_geometry_type.maxPrimitiveCount = info.GeometryType.MaxPrimitiveCount;
+	acceleration_structure_create_geometry_type.maxVertexCount = info.GeometryType.MaxVertexCount;
+	acceleration_structure_create_geometry_type.vertexFormat = static_cast<VkFormat>(info.GeometryType.VertexType);
+
+	VkAccelerationStructureCreateInfoKHR vkAccelerationStructureCreateInfoKhr{ VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_CREATE_INFO_KHR };
+	vkAccelerationStructureCreateInfoKhr.flags = info.Flags;
+	vkAccelerationStructureCreateInfoKhr.type = VK_ACCELERATION_STRUCTURE_TYPE_BOTTOM_LEVEL_KHR;
+	vkAccelerationStructureCreateInfoKhr.pGeometryInfos = &acceleration_structure_create_geometry_type;
+	vkAccelerationStructureCreateInfoKhr.maxGeometryCount = info.MaxGeometryCount;
+	vkAccelerationStructureCreateInfoKhr.compactedSize = info.CompactedSize;
+	vkAccelerationStructureCreateInfoKhr.deviceAddress = info.DeviceAddress;
+
+	info.RenderDevice->vkCreateAccelerationStructureKHR(info.RenderDevice->GetVkDevice(), &vkAccelerationStructureCreateInfoKhr, info.RenderDevice->GetVkAllocationCallbacks(), &accelerationStructure);
+	SET_NAME(accelerationStructure, VK_OBJECT_TYPE_ACCELERATION_STRUCTURE_KHR, info)
+}
+
+void GAL::VulkanAccelerationStructure::Destroy(const VulkanRenderDevice* renderDevice)
 {
 	renderDevice->vkDestroyAccelerationStructureKHR(renderDevice->GetVkDevice(), accelerationStructure, renderDevice->GetVkAllocationCallbacks());
+}
+
+void GAL::VulkanAccelerationStructure::BuildAccelerationStructure(const BuildAccelerationStructuresInfo& info)
+{
+	info.RenderDevice->vkBuildAccelerationStructureKHR(info.RenderDevice->GetVkDevice(), info.BuildAccelerationStructureInfos.ElementCount(), reinterpret_cast<const VkAccelerationStructureBuildGeometryInfoKHR*>(info.BuildAccelerationStructureInfos.begin()), reinterpret_cast<const VkAccelerationStructureBuildOffsetInfoKHR* const*>(info.BuildOffsets));
 }
